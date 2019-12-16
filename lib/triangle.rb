4f2448799316a40d9596f48165248b95fff222ce
Ebony Sharrock <ebonysharrock@gmail.com>
2019-12-16 04:11:13 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,31 +1,30 @@
 class Triangle
-  attr_accessor length1:, length2:, length3:
-  @lengths=[]
-  def initialize(length1:, length2:, length3:)
-    @lengths<<length1:
-    @lengths<<length2:
-    @lengths<<length3:
+  class TriangleError < StandardError
+    p "Lengths provided are invalid."
   end
-  class TriangeError < StandardError
-    def message
-      "Lengths provided are invalid"
-    end
+  attr_accessor :length1, :length2, :length3
+  def initialize(length1, length2, length3)
+    @length1=length1
+    @length2=length2
+    @length3=length3
+  end
+  def legal?
+    (@length1>0&&@length2>0&&@length3>0)&&
+    (@length1+@length2>@length3)&&
+    (@length2+@length3>@length1)&&
+    (@length3+@length1>@length2)
   end
   def kind
-    if (@lengths.each {|l| l>0})&&(@lengths[0]+@lengths[1]>@lengths[2])
-      if length1==length2 &&length2==length3
+    if self.legal?
+      if @length1==@length2&&@length2==@length3
         :equilateral
-      elsif length1==length2 ||length2==length3
+      elsif @length1==@length2 || @length2==@length3 || @length1==@length3
         :isosceles
       else
         :scalene
       end
     else
-      begin
-        raise TrialngleError
-      rescue TrialngleError => error
-        puts error.message
-      end
+        raise TriangleError
     end
   end
 end
