a28b5868288e390933477392c584465cd878bd35
Ebony Sharrock <ebonysharrock@gmail.com>
2019-12-16 03:42:52 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor length1:, length2:, length3:
+  @lengths=[]
+  def initialize(length1:, length2:, length3:)
+    @lengths<<length1:
+    @lengths<<length2:
+    @lengths<<length3:
+  end
+  class TriangeError < StandardError
+    def message
+      "Lengths provided are invalid"
+    end
+  end
+  def kind
+    if (@lengths.each {|l| l>0})&&(@lengths[0]+@lengths[1]>@lengths[2])
+      if length1==length2 &&length2==length3
+        :equilateral
+      elsif length1==length2 ||length2==length3
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      begin
+        raise TrialngleError
+      rescue TrialngleError => error
+        puts error.message
+      end
+    end
+  end
 end
