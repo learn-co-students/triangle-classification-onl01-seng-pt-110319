42da3cdf1cc8514837f06526bd2e82266f091f3e
Ashley Twomey <aprier091012@gmail.com>
2019-12-18 16:20:25 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_accessor :x, :y, :z
+ 
+  def initialize(x, y, z)
+  @x = x 
+  @y = y 
+  @z = z
+ 
+  def kind
+    if triangle == true && triangle2 == true 
+      puts true 
+    else 
+      raise TriangleError 
+    end 
+    
+    if x == y && y == z && x == z
+      :equilateral
+    elsif x != y && y != z && z != x
+      :scalene 
+    else 
+      :isosceles
+    end 
+  end
+  
+  def triangle 
+    x.positive? && y.positive? && z.positive?
+  end 
+  
+  def triangle2 
+    x + y > z && x + z > y && y + z > x
+  end 
+end
+
+class TriangleError < StandardError
+  "This is not a legal triangle."
 end
