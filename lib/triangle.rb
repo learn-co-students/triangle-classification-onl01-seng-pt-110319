33cf7b0c45f04d29ed1f3ffdf0c734aa33f52500
Ashley Twomey <aprier091012@gmail.com>
2019-12-20 01:25:19 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,15 +17,16 @@ class Triangle
     end
   end
   
-  def validate_triangle
+   def validate_triangle
     real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
     [a, b, c].each do |side|
       real_triangle << false if side <= 0 
-   end
-  
   end
-end 
+    raise TriangleError if real_triangle.include? (false)
+   end
+ 
 
 class TriangleError < StandardError
   "This is not a proper triangle."
+end
 end
