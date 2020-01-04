6ad70a60564afc4b14581cf07163134c1fc22592
Wills Blake <richwblake@gmail.com>
2020-01-03 20:18:22 -0500

    Final commit, all tests pass

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    sides = [@side1, @side2, @side3]
+    triangle_type = nil
+
+    if sides.find { |side| side <= 0 }
+      raise TriangleError
+    elsif !(@side1 < @side2 + @side3 && @side2 < @side1 + @side3 && @side3 < @side1 + @side2)
+      raise TriangleError
+    end
+
+    case sides.uniq.length
+    when 1
+      triangle_type = :equilateral
+    when 2
+      triangle_type = :isosceles
+    when 3
+      triangle_type = :scalene
+    end
+
+  end
+
+
+  class TriangleError < StandardError
+  end
 end
