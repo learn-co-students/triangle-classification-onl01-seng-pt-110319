647565d5db189a03ebd4e9dd2d0fc088b01bf4c4
arnie b <arnold.bwaila@gmail.com>
2020-01-13 21:48:07 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,4 @@
+
 class Triangle
   # write code here
   attr_reader :sidea, :sideb, :sidec
@@ -7,38 +8,33 @@ class Triangle
     @sideb = sideb
     @sidea = sidea
   end
-  def self.valid?
+  
+  def valid_triangle
     valid = false
-    if (((@sidea + @sideb) < @sidec) ||  ((@sidea + @sidec) < @sideb) ||  ((@sidec + @sideb) < @sidea))
-     if @sidea > 0 && sideb > 0 && sidec valid = false
+    if (((@sidea + @sideb) > @sidec) &&  ((@sidea + @sidec) > @sideb) &&  ((@sidec + @sideb) > @sidea))
+     if ((@sidea > 0) && (@sideb > 0) && (@sidec > 0))
+       valid = true
+     end
     end
     valid
   end
     
   def kind
-    if !self.valid? 
-      begin 
+    if !valid_triangle
         raise TriangleError
-      rescue TriangleError => error
-            puts error.message
-      end
-      @kind = :invalid
     else
       if ( @sidea == @sideb && @sideb == @sidec )
-        @kind = :equilateral
+        :equilateral
       elsif ( @sidea == @sideb && @sideb != @sidec )||( @sidea == @sidec && @sideb != @sidec )||( @sidec == @sideb && @sideb != @sidea )
-        @kind = :isoceles
-      elsif ( @sidea != @sideb && @sideb != @sidec && @sidec!= @sidea)
-        @kind = :scalene
+        :isosceles
+      else
+        :scalene
       end
     end
   end
-  
+ 
   class TriangleError < StandardError
-    def message
-     
-    end
+  
   end
   
-      
 end
