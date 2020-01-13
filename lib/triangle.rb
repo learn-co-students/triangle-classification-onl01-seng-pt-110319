9a31f235c10c930228639338ff34da69931219ba
arnie b <arnold.bwaila@gmail.com>
2020-01-13 00:33:10 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
   # write code here
+  attr_accessor :sidea, :sideb, :sidec, :kind
+  
+  def initiliaze(sidea, sideb, sidec)
+    @sidec = sidec
+    @sideb = sideb
+    @sidea = sidea
+  end
+  
+  def self.valid?
+    valid = true
+    if (((@sidea + @sideb) < @sidec) ||  ((@sidea + @sidec) < @sideb) ||  ((@sidec + @sideb) < @sidea))
+      valid = false
+    end
+    valid
+  end
+    
+  def kind
+    if !self.valid? 
+      begin 
+        raise TriangleError
+      rescue TriangleError => error
+            puts error.message
+      end
+    else
+      if ( @sidea == @sideb && @sideb == @sidec )
+        @kind = :equilateral
+      elsif ( @sidea == @sideb && @sideb != @sidec )||( @sidea == @sidec && @sideb != @sidec )!!( @sidec == @sideb && @sideb != @sidea )
+        @kind = :isoceles
+  end
+      
 end
